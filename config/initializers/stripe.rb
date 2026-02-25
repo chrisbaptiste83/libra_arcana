e42721c3264960284011818006fbd6f3c34e7478
Christopher Baptiste <chrisbaptiste83@gmail.com>
2026-02-25 06:45:32 -0700

    credentials dig error fix

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,7 @@
 creds = Rails.application.credentials
+
 stripe_key =
   ENV["STRIPE_SECRET_KEY"] ||
-  (creds.respond_to?(:dig) ? creds.dig(:stripe, :secret_key) : creds)
+  (creds.is_a?(String) ? creds : (creds.respond_to?(:dig) ? creds.dig(:stripe, :secret_key) : creds))
 
 Stripe.api_key = stripe_key if stripe_key.present?
