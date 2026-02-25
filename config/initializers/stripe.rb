9b76a3e1bd4e4a2babbeb3cdce4f28ff42356dfa
Christopher Baptiste <chrisbaptiste83@gmail.com>
2026-02-25 06:08:16 -0700

    fix deployment error

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,6 @@
-Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+creds = Rails.application.credentials
+stripe_key =
+  ENV["STRIPE_SECRET_KEY"] ||
+  (creds.respond_to?(:dig) ? creds.dig(:stripe, :secret_key) : creds)
+
+Stripe.api_key = stripe_key if stripe_key.present?
